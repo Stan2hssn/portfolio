e61b98df5a2ca3c8bcf8dd57cd4e904e7e95f766
Stan2hssn <86244342+Stan2hssn@users.noreply.github.com>
2023-05-29 22:00:15 +0200

    stable

--- components/atoms/parallax_images.jsx
+++ components/atoms/parallax_images.jsx
@@ -1,21 +1,51 @@
 import React, { forwardRef } from "react";
 import Image from "next/image";
 
-const ParallaxImages = forwardRef((props, ref) => (
-  <Image
-    key={props.key}
-    ref={ref}
-    className="relative min-w-[150%] transform will-change-transform bg-contain"
-    src="https://images.unsplash.com/photo-1539639087565-41c59f40f3d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
-    alt="Picture of the author"
-    width={500}
-    height={500}
-    style={{
-      transformStyle: "preserve-3d",
-      transform: "translate3d(var(--translate-x), 0, 0)",
-    }}
-    {...props}
-  />
-));
+const cover = {
+  video: "/images/projects/Liquid/Cover.mp4",
+  image: "/images/projects/Liquid/Liquid_2.webp",
+};
+
+const ParallaxImages = forwardRef((props, ref) => {
+  const isVideo = cover.video && cover.video.endsWith(".mp4");
+
+  return isVideo ? (
+    <video
+      key={props.key}
+      ref={ref}
+      width={500}
+      height={500}
+      style={{
+        transformStyle: "preserve-3d",
+        transform: "translate3D(var(--translate-x), 0,0)",
+      }}
+      autoPlay
+      muted
+      loop
+      controls
+      className="relative min-w-[150%] transform will-change-transform bg-contain"
+    >
+      <source src={cover.video} type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  ) : (
+    <Image
+      key={props.key}
+      ref={ref}
+      className="relative min-w-[130%] overflow-hidden transform will-change-transform bg-contain"
+      src={cover.image}
+      alt="Picture of the project"
+      width={500}
+      height={500}
+      priority="true"
+      quality={100}
+      style={{
+        transformStyle: "preserve-3d",
+        transform: "translate3d(var(--translate-x), 0, 0)",
+      }}
+      {...props}
+    />
+  );
+});
 
 export default ParallaxImages;
